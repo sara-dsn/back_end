cb831873d54ec4f3b9d9e1ba1239030f19d2d994
sara-dsn <sabr.tdg@gmail.com>
2023-12-18 10:25:53 +0100

    j

---
+++ add_form.php
@@ -0,0 +1,29 @@
+<form action="post">
+    <div class="form-row">
+        <label for="title"  >Title</label>
+        <input type="text" placeholder="Enter Title">
+        <label for="artist">Artist</label>
+        <select name="1"  id="1"></select>
+    </div>
+    <div class="form-row">
+        <label for="year">Year</label>
+        <input type="text"  placeholder="Enter Year">
+        <label for="genre"  >Genre</label>
+        <input type="text"placeholder="Enter genre (Rock, Pop, Prog ...)">
+    </div>
+    <div class="form-row">
+        <label for="label">Label</label>
+        <input type="text"  placeholder=" Enter Lable(EMI, Warner, PolyGram, Univers sale ...)">
+        <label for="price">Price</label>
+        <input type="text">
+    </div>
+    <div class="form-row">
+        <input type="picture">Picture</label>
+        <input type="file">
+
+</div>
+    <div class="row">
+        <button type="submit">Ajouter</button>
+        <button type="delete">Retour</button>
+    </div>
+</form>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test 2 pdo</title>
+</head>
+<body>
+    <?php 
+   
+
+   
+   try {
+       // Connexion à la base de données
+       $db = new PDO('mysql:host=localhost;charset=utf8;dbname=record', 'admin', 'dosana');
+       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+       // Exécution d'une requête SQL
+       $requete = $db->query("SELECT * FROM artist");
+       $tableau = $requete->fetchAll(PDO::FETCH_OBJ);
+       $requete->closeCursor();
+   
+       // Affichage des résultats
+       foreach ($tableau as $artist) {
+           echo $artist->artist_name . "<br>";
+       }
+   } catch (PDOException $e) {
+       echo "Erreur PDO : " . $e->getMessage() . "<br>";
+       echo "Code d'erreur PDO : " . $e->getCode() . "<br>";
+       die("Fin du script");
+   } catch (Exception $e) {
+       echo "Erreur générale : " . $e->getMessage() . "<br>";
+       echo "Code d'erreur : " . $e->getCode() . "<br>";
+       die("Fin du script");
+   }
+   
+   
+    ?>
+<?php if (!empty($tableau)): ?>
+    <?php foreach ($tableau as $artist): ?>
+        <div>
+            <?= $artist->artist_name ?>
+        </div>
+    <?php endforeach; ?>
+<?php else: ?>
+    <p>Aucun résultat trouvé.</p>
+<?php endif; ?>
+
+</body>
+</html>
